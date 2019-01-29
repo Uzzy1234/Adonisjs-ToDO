c8807e0472a86d6d5b5c2a9ddf972b48e747da00
Uzochukwu Onuegbu <uzochukwu.onuegbu@demzanalytics.com>
2019-01-29 16:35:07 +0100

    destroy method

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -2,9 +2,13 @@
 const Project = use('App/Models/Project')
 
 class ProjectController {
-  async index({ request, auth }) {
+  async index({ response, request, auth }) {
     const user = await auth.getUser()
-    return user.projects().fetch()
+    const user_projects = await user.projects().fetch()
+    return response.status(200).json({
+      status: "success",
+      data: user_projects
+    })
   }
 
   async create({ auth, request }) {
@@ -14,6 +18,24 @@ class ProjectController {
     project.title = title
     await user.projects().save(project)
     return project
+
+  }
+
+  async destroy({ response, auth, request, params }) {
+    const user = await auth.getUser()
+    const project = await Project.find(params.id)
+    if (project.user_id == user.id) {
+      project.delete()
+      return response.status(200).json({
+        status: "success",
+        data: project
+      })
+    } else {
+      return response.status(403).json({
+        status: "fail",
+        message: "not authorised"
+      })
+    }
 
   }
 }
